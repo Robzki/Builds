1ec60fa60dc59d8a102d1644b2dbe225a6bfe842
Robzki <robzkidev@gmail.com>
2016-12-23 16:17:36 +0100

    such smooth

--- async_image/ViewController.swift
+++ async_image/ViewController.swift
@@ -8,12 +8,96 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UITableViewController {
+    
+    var refreshCtrl: UIRefreshControl!
+    var tableData:[AnyObject]!
+    var task: URLSessionDownloadTask!
+    var session: URLSession!
+    var cache:NSCache<AnyObject, AnyObject>!
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        session = URLSession.shared
+        task = URLSessionDownloadTask()
+        
+        self.refreshCtrl = UIRefreshControl()
+        self.refreshCtrl.addTarget(self, action: #selector(ViewController.refreshTableView), for: .valueChanged)
+        self.refreshControl = self.refreshCtrl
+        
+        self.tableData = []
+        self.cache = NSCache()
+        
+
+    
     }
+    
+    func refreshTableView(){
+        
+        let url:URL! = URL(string:"https://api.twitch.tv/kraken/channels/KompisKlanen/videos?limit=100&client_id=ojzh3cc1s83vj46jrdgp3hkxj3honeu")
+        task = session.downloadTask(with: url, completionHandler: { (location: URL?, response: URLResponse?, error: Error?) -> Void in
+            
+            if location != nil {
+                let data:Data! = try? Data(contentsOf: location!)
+                do{
+                    let dic = try JSONSerialization.jsonObject(with: data, options: .mutableLeaves) as AnyObject
+                    self.tableData = dic.value(forKey: "videos") as? [AnyObject]
+                    DispatchQueue.main.async(execute: { () -> Void in
+                        self.tableView.reloadData()
+                        self.refreshControl?.endRefreshing()
+                    })
+                }catch{
+                    print("ingen data kom hem")
+                }
+            }
+        })
+        task.resume()
+    }
+    
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return self.tableData.count
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        
+        let cell = tableView.dequeueReusableCell(withIdentifier:"tblCell", for: indexPath)
+        let dictionary = self.tableData[(indexPath as NSIndexPath).row] as! [String:AnyObject]
+        cell.textLabel!.text = dictionary["title"] as? String
+        cell.detailTextLabel!.text = dictionary["created_at"] as? String
+        
+        
+        if (self.cache.object(forKey: (indexPath as IndexPath).row as AnyObject) != nil) {
+            print("cached image used, no need to download it")
+            cell.imageView?.image = self.cache.object(forKey: (indexPath as NSIndexPath).row as AnyObject) as? UIImage
+        }else {
+            let videoImage = dictionary["preview"] as! String
+            let url:URL! = URL(string: videoImage)
+            task = session.downloadTask(with: url, completionHandler: { (location, response, error) -> Void in
+                if let data = try? Data(contentsOf: url) {
+                    DispatchQueue.main.async(execute: { () -> Void in
+                        
+                        if let updateCell = tableView.cellForRow(at: indexPath) {
+                            let img:UIImage! = UIImage(data: data)
+                            updateCell.imageView?.image = img
+                            self.cache.setObject(img, forKey: (indexPath as NSIndexPath).row as AnyObject)
+                        }
+                })
+        }
+            })
+            task.resume()
+        }
+        
+        return cell
+    }
+    
+    
+    
+    
+    
+    
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
