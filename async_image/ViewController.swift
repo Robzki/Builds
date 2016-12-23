dd85afdb7c661878512a68bef1336de5e2637eaa
Robzki <robzkidev@gmail.com>
2016-12-23 15:36:47 +0100

    Initial Commit

---
+++ async_image/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  async_image
+//
+//  Created by Robert Elfström on 2016-12-23.
+//  Copyright © 2016 Robert Elfström. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
